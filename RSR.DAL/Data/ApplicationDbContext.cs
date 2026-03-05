eeb6d2c7c9c886288a72bf9f1c9c1ecc8270927b
sarah-shtawi <sarah.sht03@gmail.com>
2026-03-05 03:24:53 +0200

    Add project files.

---
+++ RSR.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSR.DAL.Data
+{
+    public  class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options):base(options) { }
+
+    }
+}
