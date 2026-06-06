f064821a02521b9bf3a4692f997096ba36467a9f
sarah-shtawi <sarah.sht03@gmail.com>
2026-06-06 16:10:22 +0300

    Hidden Data when semester end

--- RSR.DAL/Data/ApplicationDbContext.cs
+++ RSR.DAL/Data/ApplicationDbContext.cs
@@ -59,6 +59,14 @@ namespace RSR.DAL.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            // hide when the semester ended 
+            modelBuilder.Entity<Group>().HasQueryFilter(g => g.Semester.IsActive);
+            modelBuilder.Entity<Task>().HasQueryFilter(t => t.Group.Semester.IsActive);
+            modelBuilder.Entity<TaskSubmission>().HasQueryFilter(ts => ts.Task.Group.Semester.IsActive);
+            modelBuilder.Entity<Schedule>().HasQueryFilter(s => s.Group.Semester.IsActive);
+            modelBuilder.Entity<StudentProfile>().HasQueryFilter(s => s.Group.Semester.IsActive);
+
+
             // Change Names of Identity Tables
             modelBuilder.Entity<ApplicationUser>().ToTable("Users");
             modelBuilder.Entity<IdentityRole>().ToTable("Roles");
